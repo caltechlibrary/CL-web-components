42a6f8b661c6bdc67640ce676cde15d2d65e8801
Twila Smith <7118907+camp5456@users.noreply.github.com>
2025-11-13 12:20:55 -0600

    fixing typo

--- footer-global.js
+++ footer-global.js
@@ -127,7 +127,7 @@ var FooterGlobal = class extends HTMLElement {
           flex-wrap: wrap;
           justify-content: center;
           width: 100%;
-          gap:5em
+          gap:5em;
         }
         
         :host([custom]) .footer-column {
